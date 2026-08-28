61a7aacad5650508ac2d69d2a0448615d289dd5e
Alexey Khoroshenkiy <alexeyk4@nycstudents.net>
2026-08-28 13:05:23 -0400

    make misc config

--- niri/default.nix
+++ niri/default.nix
@@ -13,7 +13,27 @@
     package = pkgs.niri;
 
     settings = {
+      spawn-at-startup = [
+        { argv = [ "glide" ]; }
+      ];
+
       hotkey-overlay.skip-at-startup = true;
+
+      prefer-no-csd = true;
+
+      cursor = {
+        hide-when-typing = true;
+        hide-after-inactive-ms = 1000;
+      };
+
+      clipboard.disable-primary = true;
+
+      workspaces = {
+        "terminals" = { };
+        "browser" = { };
+        "terminals" = { };
+        "misc" = { };
+      };
     };
   };
 }
