70e0633864542eac540feae9195fa456d22ff547
Alexey Khoroshenkiy <alexeyk4@nycstudents.net>
2026-08-28 11:15:52 -0400

    make binds config

--- niri/default.nix
+++ niri/default.nix
@@ -4,10 +4,15 @@
   imports = [
     ./input.nix
     ./layout.nix
+    ./binds.nix
   ];
 
   programs.niri = {
     enable = true;
     package = pkgs.niri;
+
+    settings = {
+      hotkey-overlay.skip-at-startup = true;
+    };
   };
 }
